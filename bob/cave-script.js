87cb4be1b0e710e5a45b5754bed89aecc1a9cca8
rbunge-nsc <robert.bunge@seattlecolleges.edu>
2021-01-23 18:25:47 -0800

    Moving the script to a js file.

---
+++ bob/cave-script.js
@@ -0,0 +1,22 @@
+var myimages = ["images/testpattern.jpg", "images/wandavision.jpg", "images/mando.jpg", "images/doom-patrol-show.jpg", "images/sabrina.jpg"];
+
+var currentImage = 0;
+
+function buildImages() {
+    for (var i = 0; i < myimages.length; i++) {
+       document.getElementById("thepic").src=images[i++];
+   }
+}
+
+function goHigher() {
+  currentImage++;
+  currentImage = currentImage%5;
+  document.getElementById("demo").setAttribute("src",myimages[currentImage]);
+}
+
+function goLower() {
+  currentImage--;
+  if(currentImage<0){currentImage=4;}
+  else{currentImage = currentImage%5;}
+  document.getElementById("demo").setAttribute("src",myimages[currentImage]);
+}
